843fcaa16167cbddce0fa0a3223dfe4196a1e8f6
Esther Mosaku <mosakukorede12@gmail.com>
2026-08-24 20:17:50 -0600

    additions to the leaderboard

--- src/leaderboard.c
+++ src/leaderboard.c
@@ -1,7 +1,27 @@
 #include "leaderboard.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int leaderboard_load(ScoreEntry entries[], int max_entries){
-    //TODO
+    FILE *fp = fopen(LEADERBOARD_FILE, "rb");
+    if (fp == NULL){
+        return 0;
+    }
+    size_t count = fread(entries, sizeof(ScoreEntry), max_entries, fp);
+    fclose(fp);
+
+    return count;
 }
+
+void insert_score(ScoreEntry entries[], ScoreEntry new_entry, int *count){
+    for (int i = 0; i < entries; i++){
+        if (new_entry.score > entries[i].score){
+        
+        }
+    }
+}
+
+
+
+
 
